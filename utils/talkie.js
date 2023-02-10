fe5c0905fcef7089cab594679b862af6b0fad8a4
fancn21th <fancn21th@gmail.com>
2023-02-10 17:48:30 +0800

    code refactor

--- utils/talkie.js
+++ utils/talkie.js
@@ -8,31 +8,35 @@ const spinner = ora({ text: `` });
 
 const log = console.log;
 
+const callAi = async (prompt, apiKey) => {
+	spinner.start(`\n${y('OpenAI')} 正在思考...\n\n${d('请等待...')}`);
+
+	let response = await ai({ prompt, apiKey });
+
+	let result = null;
+
+	if (Array.isArray(response.data.choices)) {
+		result = response.data.choices[0].text;
+	} else {
+		result = response.data.choices.text;
+	}
+
+	spinner.stop();
+
+	log({ result });
+
+	return result;
+};
+
 module.exports = async () => {
 	let keys = await questionsApiKey();
 	let vars = await questions();
 
-	spinner.start(`\n${y('OpenAI')} 正在思考...\n\n${d('请等待...')}`);
-
-	let response = await ai({ prompt: vars.q, apiKey: keys.apiKey });
-
-	spinner.stop();
+	await callAi(vars.q, keys.apiKey);
 
 	while (true) {
-		if (Array.isArray(response.data.choices)) {
-			result = response.data.choices[0].text;
-		} else {
-			result = response.data.choices.text;
-		}
-
-		log({ result });
-
 		vars = await questions();
 
-		spinner.start(`\n${y('OpenAI')} 正在思考...\n\n${d('请等待...')}`);
-
-		response = await ai({ prompt: vars.q });
-
-		spinner.stop();
+		await callAi(vars.q);
 	}
 };
